6eee85955b728a1dc9c9846c6caea1f171eb1722
Sonia Gupta <soniagupta@Sonias-Air.attlocal.net>
2025-04-02 00:09:57 -0400

    Render featured puppy info on click

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,38 @@ import { puppyList } from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  
+  const [featPupId, setFeatPupId] = useState(null);
   console.log("puppyList: ", puppyList);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("Featured puppy: ", featuredPup);
+
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })
-      }
-  </div>
+      {puppies.map((puppy) => {
+        return (
+          <p 
+            onClick={() => {
+              console.log("puppy id: ", puppy.id);
+              setFeatPupId(Number(puppy.id));
+            }}
+            key ={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+
+      {featPupId && featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
