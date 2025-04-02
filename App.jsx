b66f7c43da27475b5531ea15a6c32023bd76f48d
Sonia Gupta <soniagupta@Sonias-Air.attlocal.net>
2025-04-01 20:21:19 -0400

    Display puppy names using map and useState
    # Conflicts:
    #	App.jsx

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { useState } from 'react'
+import { puppyList } from './data.js'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  
+  console.log("puppyList: ", puppyList);
+  return (
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })
+      }
+  </div>
+  );
+}
+
+export default App;
